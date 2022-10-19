fd4fc252e880552964f62b9e035ef1d13f1a5a21
Cryptoleek13 <mjackson10396@icloud.com>
2022-10-19 14:08:58 -0400

    Added server arc

--- main.go
+++ main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello World from Go!")
+	args := os.Args
+
+	if len(args) < 2 {
+		fmt.Printf("Usage: ./http-get <url>")
+		os.Exit(1)
+	}
+
+	if _, err := url.ParseRequestURI(args[1]); err != nil {
+		fmt.Printf("Url is in invalid format: %s\n", err)
+		os.Exit(1)
+	}
+
+	response, err := http.Get(args[1])
+
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+
+	defer response.Body.Close()
+
+	body, err := io.ReadAll(response.Body)
+	if err != nill {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("HTTP Status Code: %d\nBody: %s\n", response.StatusCode, body)
+
 }
