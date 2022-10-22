0b73be656ef217f49fe8c39b9845e488a6337f7d
Cryptoleek13 <mjackson10396@icloud.com>
2022-10-22 11:08:00 -0400

    Bufio

--- DerekBanasCourse/hellogo.go
+++ DerekBanasCourse/hellogo.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+var pl = fmt.Println
 
 func main() {
-	fmt.Println("Hello Go")
+	pl("What is your name")
+	reader := bufio.NewReader(os.Stdin)
+	name, err := reader.ReadString('\n')
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	pl("Hello", name)
 }
